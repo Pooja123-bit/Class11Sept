6f70df9cf403c99d06a48ac066da8e864078b85e
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-09-11 15:55:51 -0500

    edit

---
+++ Class11Sept.R
@@ -0,0 +1,18 @@
+dim (e.lat)
+str(e.lat)
+e.lat <- fish[fish$area_fat == "East",}$parcel.start.lat
+fish_lg_patch <- fish[fish$parcel.length.m >= 20,]
+install.packages("data table")
+#Go this URL and download to fix: https://cran.r_project.org/bin/windows/Rtools/index.html
+
+write a function to install package.skeleton(packs <- c("tidyverse","stringr","plyr","reshape2","grid","gridExtra","pastecs","lubridate")
+                                             n =length(packs)
+                                             
+                                             #loop through installations
+                                             for(i in 1:n){}
+                                               install.packages(packs[n])
+                                               }
+#read-in data from a text file ----
+file <- list.files(full.names = TRUE, pattern = ",txt")
+
+                                             
