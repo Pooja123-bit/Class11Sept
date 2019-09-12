86dead73893b40eee292ce49d9c358b4f3832b72
Pooja123-bit <poojaupadhyay100@gmail.com>
2019-09-12 13:44:10 -0500

    Dr.Robbinson extraclass

--- Class11Sept.R
+++ Class11Sept.R
@@ -15,4 +15,45 @@ write a function to install package.skeleton(packs <- c("tidyverse","stringr","p
 #read-in data from a text file ----
 file <- list.files(full.names = TRUE, pattern = ",txt")
 1
-                                             
+
+v<-c(1,2,3,5,6); z<-c(5,10,15,20); w<-c(3,6,9)
+v
+Z
+Z[3]                                             
+m<-matrix(c(1001:1004), nrow=2, ncol=2)
+m
+m[1,1]
+m[1,2]
+m[1,2] <- 1025
+m
+m[1,2] = 1025
+m
+l <- list(a=1,b=2,c=3,d=4,e=5)
+l
+m
+str(m)
+class(m)
+a<- array(data= 1:24, dim=c(3,4,2))
+a
+a[2,2,2]
+
+load("fish_data.Rdata")
+head(fish)
+names(fish)
+str(fish$area_fac)
+summary(fish$parcel.end.lat)
+p<-fish[fish$parcel.end.lat<25,]
+summary(p$parcel.end.lat)
+o<-fish[fish$depth_fac=="Shallow",]
+unique(o$depth_fac)
+m<-fish[fish$year=="OST14",]
+unique(m$year)
+install.packages("data.table")
+install.packages("devtools")
+install.packages(c("tidyverse","stringr","plyr","reshape2","grid",
+                   "gridExtra","patecs","lubridadte"))
+library(devtools)
+install_github("Displayr/flipPlots")
+update.packages(c("tidyverse","stringr","plyr","reshape2","grid",
+                   "gridExtra","patecs","lubridadte"))
+1
